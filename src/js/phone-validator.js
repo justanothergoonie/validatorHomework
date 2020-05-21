cf08793dd0fd16114634fe3919f003c3a20baeae
Cody J Powell <codyj.powell@outlook.com>
2020-05-21 15:34:59 -0400

    inital commit

---
+++ src/js/phone-validator.js
@@ -0,0 +1,30 @@
+class PhoneValidator extends Validator {
+	constructor(parameters) {
+		super(parameters);
+		console.log('PhoneValidator()', this.options);
+
+		console.log(
+			'looking for non-american phones?',
+			this.options.nonAmerican
+		);
+	}
+
+	validate(event) {
+		console.log('phoneValidator.validate', event);
+		super.validateBefore(event);
+		console.log('running validation for PhoneValidator', event);
+
+		const value = event.target.value;
+		console.log('value', value);
+		if (/^\s*\(?\d{3}\)?[\-\. ]?\d{3}[\-\. ]?\d{4}$\s*$/.test(value)) {
+			console.log('valid phone number!');
+			this.isValid = true;
+			event.target.classList.remove('invalid');
+		} else {
+			this.isValid = false;
+			console.log('not a valid phone number');
+		}
+
+		super.validateAfter(event);
+	}
+}
